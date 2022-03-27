e2afb8592971f59f22925dbd4b0ec9aa5d5d3769
k414bin <k414bin@yandex.ru>
2022-03-27 17:26:14 +0300

    Function fib() added

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -12,13 +12,22 @@ namespace TestWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        /* запуск программы  */
         public MainWindow()
         {
             InitializeComponent();
-            Dispatcher.Invoke(() => Title = "Time to start program " + DateTime.Now.ToString());
+          
+            
+            Dispatcher.Invoke(() => Title = "Time to start program " + __SyncTime.ToString());
+
         }
 
+        /* Создание объекта только для чтения для lock */
         private static readonly object __SyncRoot = new object();
+
+        /* Создание объекта только для чтения для отчета секунд, когда запустится новый поток  */
+        /* Не понадобился, потому что потоки не переключаются у меня */
+        private static readonly DateTime __SyncTime = DateTime.Now;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -37,26 +46,40 @@ namespace TestWPF
 
             thread[i] = new Thread(() =>
             {
-                lock   (__SyncRoot) { 
-                Application.Current.Dispatcher.BeginInvoke(() =>
+                lock   (__SyncRoot)
+                { 
+                    this.Dispatcher.BeginInvoke(() =>
                 {
-                    var result = "\n" + Thread.CurrentThread.ManagedThreadId.ToString() + " " + Thread.CurrentThread.ThreadState.ToString() +" " + DateTime.Now.ToString();
+                    textBoxes[i].Text = string.Empty;
+                    var result =  Thread.CurrentThread.ManagedThreadId.ToString() + " " +fib(21).ToString()+" "+ DateTime.Now.ToString() + Environment.NewLine; ;
                      textBoxes[i].Text = result;
                                 
                 });
                 }                     
             });
-            
 
             thread[i].IsBackground = true;
+            /* Добавляем в список поток */
             threads_list.Add(thread[i]); 
             }
 
+            /* Запуск всех потоков  из списка */
             foreach (var threads in threads_list) 
             {   
                     threads.Start();    
                     Thread.Sleep(Timeout);
             }
+                    
         }
+        /* Расчет 21 числа Фибоначчи */
+        static decimal fib(decimal n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            return fib(n - 1) + fib(n - 2);
+
+        }
+
     }
 }
