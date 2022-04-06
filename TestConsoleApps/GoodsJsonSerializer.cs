46bf4fb6b07860d4e17bc5afa7a462b43968ccc4
k414bin <k414bin@yandex.ru>
2022-04-06 15:55:01 +0300

    Json De/Serialize structures

---
+++ TestConsoleApps/GoodsJsonSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace TestConsoleApps
+{
+    internal class GoodsJsonSerializer : GoodsSerializer
+    {
+        public override void Serialize(Stream stream, List<Goods> goods)
+        {
+            JsonSerializer.Serialize(stream,goods);
+        }
+
+        public override List<Goods> Deserialize(Stream stream)
+        {
+            return JsonSerializer.Deserialize<List<Goods>>(stream)
+             ?? throw new InvalidOperationException("Не удалось получить объект класса Список товаров");
+        }
+
+    }
+}
