7051617f9ef6ba30b6b80a8bb57fda8152abbfa7
k414bin <k414bin@yandex.ru>
2022-04-18 18:53:55 +0300

    Both reports created

---
+++ Tests/TestRazor/ReportExamples/Infrastructure/FileInfoEx.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ReportExamples.Infrastructure
+{
+    internal static  class FileInfoEx
+    {
+        public static Process? Execute(this FileInfo file, bool UseShellExecute = true)
+        {
+            var info = new ProcessStartInfo(file.FullName) { UseShellExecute = UseShellExecute };   
+            return Process.Start(info); 
+
+        }
+    }
+}
---
+++ Tests/TestRazor/ReportExamples/Interfaces/IReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportExamples.Interfaces
+{
+    public interface IReport
+    {
+        public string OsVersion { get; set; }
+        public bool Os64 { get; set; }
+        public string PcName { get; set; }
+        public int NumberOfCpus { get; set; }
+        public string WindowsFolder { get; set; }
+        public string[] LogicalDrives { get; set; }
+
+        FileInfo Create (string ReportTemplateFile);
+    }
+}
