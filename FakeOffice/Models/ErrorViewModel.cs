4ff2994dc1034a3944f9aa458e9e742710c5928f
k414bin <k414bin@yandex.ru>
2022-04-20 22:49:04 +0300

    New project added

---
+++ FakeOffice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FakeOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
